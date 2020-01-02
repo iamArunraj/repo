836e4fb3d22bdc7fbdaa7432ec28038c06afd53b
Padicherry <Arunraj.Padicherry@sunquestinfo.com>
2020-01-02 16:09:45 +0530

    new

---
+++ jsonEdit.ps1
@@ -0,0 +1,14 @@
+﻿#
+# This script is for updating the 'apiUrl' value in the config.json file
+# Script takes path to the config.json file and the new value for 'apiUrl'
+# Script will finally commit the changes to the json config file
+#
+
+Param(
+[Parameter(Mandatory = $true, HelpMessage = "Path to the *.json config file")]$JsonPath,
+[Parameter(Mandatory = $true, HelpMessage = "New url data for apiURL")]$apiURL
+)
+
+$configData = Get-Content -Raw -Path $JsonPath | ConvertFrom-Json
+$configData.apiUrl = $apiURL
+$configData | ConvertTo-Json | Set-Content $JsonPath
